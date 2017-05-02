0a5b771267f5c3b5b606980bd4b12a9e235c70f6
Bharath Kumar Narahari <naraharibharathkumar@gmail.com>
2017-05-01 23:51:53 -0700

    Deleting the Python version of Apriori

--- apriori Algorithm/apriori_calc.py
+++
@@ -1,9 +0,0 @@
-import breakData
-import generateRules
-
-urlSet, transactionList = breakData.break_data()
-print urlSet
-print ".................."
-print transactionList
-items, rules = generateRules.generate_rules(urlSet, transactionList, 0.17, 0.8)
-generateRules.printResults(items, rules)
