384ef3564efa31253c0d821b963115ecb7c40dde
Bharath Kumar Narahari <naraharibharathkumar@gmail.com>
2017-05-04 21:55:12 -0700

    Added improvements to add websites to more than one slot based on timestamps, generated time spent based on category.

--- AprioriRulesGenerator/src/main/java/HomeClass.java
+++ AprioriRulesGenerator/src/main/java/HomeClass.java
@@ -3,33 +3,70 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 
-import java.net.UnknownHostException;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by maverick on 5/1/17.
  */
+
 public class HomeClass {
+    // historyList - HashMap for slot, url-time
+    // transactionList - List of transactions
+    // categoryList - HashMap for url, category
+
     public static void main(String[] args){
         MongoHelper mongoObject = new MongoHelper();
         try{
-            WebCategorizer wc = new WebCategorizer();
-            ArrayList<String> ab = new ArrayList<>();
-            ab.add("facebook.com");
-            ab.add("orkut.com");
-            ab.add("google.com");
-            wc.getWebCategories(ab);
             /*
             // Get Users list from MongoDb
             mongoObject.connect();
-            // Get previous day browsing data for each User
 
-            // Generate transactions from browsing data
+            // Get previous day browsing data for each User
+            */
+
+            // Break data into time slots and time spent
             HashMap<Integer, JSONObject> historyList = breakData();
+            System.out.println(historyList);
+
+            // Retrieve category of the url
+            HashSet<String> urlList = new HashSet<String>();
+            for(int i : historyList.keySet()) {
+                urlList.addAll(historyList.get(i).keySet());
+            }
+            WebCategorizer wc = new WebCategorizer();
+            HashMap<String, String> categoryList = wc.getWebCategories(urlList);
+            for(String s : categoryList.keySet()){
+                System.out.println("URL: "+s+"; Category: "+categoryList.get(s));
+            }
+
+            // Generate time spent on each category in a time slot
+            HashMap<Integer, JSONObject> urlTimeMap = new HashMap<Integer, JSONObject>();
+            for(int i : historyList.keySet()){
+                JSONObject urlData = historyList.get(i);
+                for (Object url : urlData.keySet()) {
+                    JSONObject tempJSON = new JSONObject();
+                    String currentUrl = (String) url;
+                    String category = categoryList.get(currentUrl);
+                    System.out.println(category);
+                    if(urlTimeMap.containsKey(i)){
+                        tempJSON = urlTimeMap.get(i);
+                        if(tempJSON.containsKey(category)){
+                            tempJSON.put(category, (long)tempJSON.get(category) + (long)urlData.get(currentUrl));
+                        }
+                        else {
+                            tempJSON.put(category, urlData.get(currentUrl) );
+                        }
+                    }
+                    else {
+                        tempJSON.put(category, urlData.get(currentUrl) );
+                        urlTimeMap.put(i, tempJSON);
+                    }
+                }
+            }
+            System.out.println(urlTimeMap);
+
+            // Generate transactional data from historyList
             List<String> transactionList = new ArrayList<String>();
             for(int i : historyList.keySet()){
                 JSONObject tempJSON = historyList.get(i);
@@ -43,34 +80,37 @@ public class HomeClass {
                     transactionList.add(tempTransaction.toString());
             }
             transactionList.set(1,"newtab mongodb.github.io");
-
+            /*
             for (String s: transactionList
                     ) {
                 System.out.println(s);
             }
-            // Generate rules
+            */
 
+            /*
+            // Generate rules
             GenerateRules gr = new GenerateRules();
             ArrayList<JSONObject> rules = gr.generateRules(transactionList);
             for(JSONObject rule:rules) {
                 System.out.println(rule);
             }
-
-            // Save rules in MongoDb
-
-
-
+            */
+            // Save rules & url category count in MongoDb
+            /*
             mongoObject.disconnect();
             */
         }
         catch(Exception ex) {
             if(ex.getClass().getName().equals("UnknownHostException")){
-                mongoObject.disconnect();
                 System.out.println("Error in connecting to MongoDb");
             }
         }
+        finally {
+            mongoObject.disconnect();
+        }
     }
 
+    // Function to break data set into HashMap
     private static HashMap<Integer, JSONObject> breakData(){
         //Dummy Data
         String s = "[" +
@@ -184,6 +224,7 @@ public class HomeClass {
         }
     }
 
+    // Function to generate slots for url
     private static void addToSlot(HashMap<Integer, JSONObject> historyList, JSONObject historyRecord){
         long t1 = (long) historyRecord.get("T1");
         long t2 = (long) historyRecord.get("T2");
@@ -191,6 +232,29 @@ public class HomeClass {
         Timestamp T2 = new Timestamp(t2);
         int key = T1.getHours();
         String url = (String)historyRecord.get("hostname");
+        Timestamp T3 = T1;
+        while(T3.getHours() != T2.getHours()) {
+            //System.out.println("URL  :"+url+"; T3 :"+T3+"; T2 :"+T2);
+            T3 = addTransaction(url,T3,T2,historyList,historyRecord);
+        }
+        if(T3!=T2){
+            //System.out.println("URL :"+url+"; T3 :"+T3+"; T2 :"+T2);
+            if(historyList.containsKey(key/2)){
+                JSONObject tempJSON = historyList.get(key/2);
+                if(tempJSON.containsKey(url)){
+                    tempJSON.put(url, (Long)tempJSON.get(url) + (T2.getTime()-T3.getTime())/1000);
+                }
+                else{
+                    tempJSON.put(url, (T2.getTime()-T3.getTime())/1000);
+                }
+            }
+            else{
+                JSONObject tempJSON = new JSONObject();
+                tempJSON.put(url, (T2.getTime()-T3.getTime())/1000 );
+                historyList.put(key/2, tempJSON);
+            }
+        }
+        /*
         if(historyList.containsKey(key/2)){
             JSONObject tempJSON = (JSONObject) historyList.get(key/2);
             if(tempJSON.containsKey(url)){
@@ -205,5 +269,39 @@ public class HomeClass {
             tempJSON.put(url, (t2-t1)/1000 );
             historyList.put(key/2, tempJSON);
         }
+        */
+    }
+
+    // Function to add url to a specific transaction slot
+    private static Timestamp addTransaction(String url, Timestamp T1,  Timestamp T2, HashMap<Integer, JSONObject> historyList, JSONObject historyRecord) {
+        int key = T1.getHours();
+        Timestamp T3 = null;
+        if(T2.getHours() - T1.getHours() > 2) {
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(T1.getTime());
+            cal.add(Calendar.HOUR, 2);
+            cal.add(Calendar.MINUTE, 60-T1.getMinutes());
+            cal.add(Calendar.SECOND, 59-T1.getSeconds());
+            T3 = new Timestamp(cal.getTime().getTime());
+            System.out.println(T3);
+        } else {
+            T3 = T2;
+        }
+        if(historyList.containsKey(key/2)){
+            JSONObject tempJSON = historyList.get(key/2);
+            if(tempJSON.containsKey(url)){
+                tempJSON.put(url, (Long)tempJSON.get(url) + (T3.getTime()-T1.getTime())/1000);
+            }
+            else{
+                tempJSON.put(url, (T3.getTime()-T1.getTime())/1000);
+            }
+        }
+        else{
+            JSONObject tempJSON = new JSONObject();
+            tempJSON.put(url, (T3.getTime()-T1.getTime())/1000 );
+            historyList.put(key/2, tempJSON);
+        }
+
+        return T3;
     }
 }
